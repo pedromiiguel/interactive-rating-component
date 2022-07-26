f4640a0b4b50e1a5788ea1b1e19fedbdb636124b
Pedro Miguel <pedromrap@gmail.com>
2022-07-26 16:22:35 -0300

    feat: added jest configs

---
+++ jest.config.js
@@ -0,0 +1,11 @@
+// eslint-disable-next-line no-undef
+module.exports = {
+  testEnvironment: 'jsdom',
+  testPathIgnorePatterns: ['/node_modules/', '/.next/'],
+  collectCoverage: true,
+  collectCoverageFrom: ['src/**/*.ts(x)'],
+  setupFilesAfterEnv: ['<rootDir>/.jest/setup.ts'],
+  moduleNameMapper: {
+    '\\.css$': 'identity-obj-proxy'
+  }
+}
